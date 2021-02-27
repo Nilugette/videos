16cf23af37b0ecbd96b8cdf9f6036f6cba8ad97a
Nilugette <nilugette@yahoo.fr>
2021-02-27 18:12:08 +0100

    Defaulting Video Selection

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -4,7 +4,7 @@ import VideoItem from './VideoItem'
 const VideoList = ({ videos, onVideoSelect }) => {
     //console.log(props.images)
        const renderedList = videos.map(video => {
-            return <VideoItem onVideoSelect={onVideoSelect} key={video.id} video={video} />
+            return <VideoItem onVideoSelect={onVideoSelect} key={video.id.videoId} video={video} />
         })
         
         return <div className="ui relaxed divided list">{renderedList}</div>
